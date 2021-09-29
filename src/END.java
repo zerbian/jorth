dc711bc3ca42a6a6678a410d3a2806631c8f1960
zerbian <zerbian@campus.tu-berlin.de>
2021-09-29 12:01:50 +0200

    change end

--- src/END.java
+++ src/END.java
@@ -1,10 +1,16 @@
 import java.util.Stack;
 
-public class END implements Operation {
+public class End implements Operation {
+
+    Object returnReference = null;
 
     @Override
     public int execute(Stack<Integer> s) {
         return 0;
+    }
+
+    public Object getReference() {
+        return returnReference;
     }
     
 }
